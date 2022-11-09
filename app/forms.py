5070be041daf642eeb0681c3d2177d826114daca
Dmitry <budilovdmi@gmail.com>
2022-11-10 04:39:15 +1200

    update

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,39 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields import StringField, PasswordField, BooleanField, SubmitField, DateField
+from wtforms.validators import DataRequired, EqualTo, ValidationError
+
+from app.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    birthday = DateField('Birthday', format="%Y-%m-%d")
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    birthday = DateField('Birthday', format="%Y-%m-%d")
+    email = StringField('Email', validators=[DataRequired()])
+    old_password = PasswordField('Old password', validators=[DataRequired()])
+    password = PasswordField('New password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+    def validate_old_pass(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if not user.chek_password(old_password.data):
+            raise ValidationError('Old password not valid!')
--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,19 @@
+from datetime import datetime as dt
+
 from flask import render_template, redirect, flash, url_for, request
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 
-from app import app
-from app.forms import LoginForm
+from app import app, db
+from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User, Post
+
+
+@app.before_request
+def before_request():
+    if current_user.is_authenticated:
+        current_user.last_seen = dt.utcnow()
+        db.session.commit()
 
 @app.route('/')
 @app.route('/index/')
@@ -38,18 +47,40 @@ def logout():
     logout_user()
     return redirect(url_for('index'))
 
-@app.route('/menu/')
-def menu():
-    return render_template('menu.html')
+@app.route('/users/', methods=['GET', 'POST'])
+@login_required
+def users():
+    form = RegistrationForm()
+    users = User.query.all()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, 
+                    surname=form.surname.data,
+                    email=form.email.data,
+                    birthday=form.birthday.data
+                    )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered!')
+        return redirect(url_for('users'))
+    return render_template('users.html', form=form, users=users)
 
-@app.route('/dashboard/')
-def dashboard():
-    return render_template('dashboard.html')
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = user.posts
+    return render_template('user.html', user=user, posts = posts)
 
-@app.route('/file_list/')
-def file_list():
-    return render_template('file_list.html')
-
-@app.route('/settings/')
-def settings():
-    return render_template('settings.html')
+@app.route('/edit_profile/<username>/', methods=['GET', 'POST'])
+def edit_profile(username):
+    form = EditProfileForm()
+    user = User.query.filter_by(username=username).first()
+    if form.validate_on_submit():
+        return redirect(url_for('edit_profile', username=user.username))
+    elif request.method == 'GET':
+        form.username.data = user.username.title()
+        form.surname.data = user.surname
+        form.email.data = user.email
+        form.birthday.data = user.birthday
+    return render_template('edit_profile.html', form=form, user=user)
